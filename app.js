57d79e5a7338f4928ec8f0ffbc2db0c222200b31
Aung Ye Htet <aung.atg1738@gmail.com>
2024-04-04 21:53:53 +0630

    SetUp for SANote

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
-const postRoutes = require("./routes/post");
+require("dotenv").config();
 
 const app = express();
 
-// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({}));
 
-app.use(postRoutes);
-
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
-});
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then((_) => {
+    app.listen(8000);
+    console.log("Database Connected & Server is running on port 8000");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
