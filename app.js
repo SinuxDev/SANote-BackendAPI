863fbaf88e0cb412ca63448d5d68909f51168b56
Aung Ye Htet <aung.atg1738@gmail.com>
2024-04-08 14:34:04 +0630

    Set Multer in Server

--- app.js
+++ app.js
@@ -2,13 +2,41 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const multer = require("multer");
 require("dotenv").config();
 
 const app = express();
 
 const noteRoutes = require("./routes/note");
 
+const stroageConfigure = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "Uploads");
+  },
+  filename: (req, file, cb) => {
+    const suffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, suffix + "-" + file.originalname);
+  },
+});
+
+const fileFilterConfigure = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, undefined);
+  }
+};
+
 app.use(bodyParser.json());
+app.use(
+  multer({ storage: stroageConfigure, fileFilter: fileFilterConfigure }).single(
+    "cover_image"
+  )
+);
 app.use(cors({}));
 
 app.use(noteRoutes);
